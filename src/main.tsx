34807a221176d5454acba8d6dc5079499223f4d7
Johnny <johnnywebster0117@gmail.com>
2023-12-22 16:30:46 +0900

    Layout rebuild

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "@/app/store";
+import { store } from "@/redux/store.ts";
 
 import App from "./App.tsx";
 import "./index.css";
